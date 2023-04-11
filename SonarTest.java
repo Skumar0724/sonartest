eda446a88bb4b19e0876be18246837a3787e2397
Skumar0724 <126073255+Skumar0724@users.noreply.github.com>
2023-04-11 18:44:53 +0530

    Add files via upload

---
+++ SonarTest.java
@@ -0,0 +1,12 @@
+package test.sonar;
+
+public class SonarTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello >>>> ");
+		int a = 1;
+		int b = 0;
+		System.out.println("a/b "+(a/b));
+	}
+
+}
---
+++ TestSonar.java
@@ -0,0 +1,10 @@
+package test.sonar;
+
+public class TestSonar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
